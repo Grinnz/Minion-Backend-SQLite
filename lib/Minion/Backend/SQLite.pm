15a159dd29feb8d448d84a9dc9b2539c28ab3277
Dan Book <grinnz@grinnz.com>
2015-10-28 16:46:13 -0400

    initial commit

---
+++ lib/Minion/Backend/SQLite.pm
@@ -0,0 +1,35 @@
+package Minion::Backend::SQLite;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+=head1 NAME
+
+Minion::Backend::SQLite - Module abstract
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 BUGS
+
+Report any issues on the public bugtracker.
+
+=head1 AUTHOR
+
+Dan Book <dbook@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2015 by Dan Book.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=head1 SEE ALSO
+
