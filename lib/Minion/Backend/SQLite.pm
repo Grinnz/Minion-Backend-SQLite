ee406b61fb3673b416ffdc3ea7973f6a26cb6af1
Dan Book <grinnz@grinnz.com>
2017-11-17 16:33:58 -0500

    this will be version 3.000, regenerate meta files

--- lib/Minion/Backend/SQLite.pm
+++ lib/Minion/Backend/SQLite.pm
@@ -8,7 +8,7 @@ use Mojo::Util 'steady_time';
 use Sys::Hostname 'hostname';
 use Time::HiRes 'usleep';
 
-our $VERSION = '2.005';
+our $VERSION = '3.000';
 
 has dequeue_interval => 0.5;
 has 'sqlite';
