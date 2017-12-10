34f1c16afef37fc80189f8dd9a543c2b6349cef3
Dan Book <grinnz@grinnz.com>
2017-12-10 16:00:45 -0500

    Bump version

--- lib/Minion/Backend/SQLite.pm
+++ lib/Minion/Backend/SQLite.pm
@@ -8,7 +8,7 @@ use Mojo::Util 'steady_time';
 use Sys::Hostname 'hostname';
 use Time::HiRes 'usleep';
 
-our $VERSION = '3.003';
+our $VERSION = '3.004';
 
 has dequeue_interval => 0.5;
 has 'sqlite';
